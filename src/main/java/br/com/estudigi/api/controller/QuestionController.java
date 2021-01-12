bd663bc396f0f9c62dd831d91618b8256791dc58
Emerson Bezerra <emersondevelops@gmail.com>
2021-01-12 18:54:13 -0300

    Question management fully functional.

--- src/main/java/br/com/estudigi/api/controller/QuestionController.java
+++ src/main/java/br/com/estudigi/api/controller/QuestionController.java
@@ -1,20 +1,33 @@
 package br.com.estudigi.api.controller;
 
+import br.com.estudigi.api.model.Choice;
 import br.com.estudigi.api.model.Question;
+import br.com.estudigi.api.repository.ChoiceRepository;
 import br.com.estudigi.api.repository.QuestionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
 
-    final
-    QuestionRepository questionRepository;
+    final QuestionRepository questionRepository;
+    final ChoiceRepository choiceRepository;
 
-    public QuestionController(QuestionRepository questionRepository) {
+    public QuestionController(QuestionRepository questionRepository, ChoiceRepository choiceRepository) {
+
         this.questionRepository = questionRepository;
+        this.choiceRepository = choiceRepository;
+    }
+
+    @PostMapping
+    @CrossOrigin
+    public ResponseEntity<Question> create(@RequestBody Question question) {
+        Question newQuestion = questionRepository.save(question);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newQuestion);
     }
 
     @GetMapping
@@ -23,10 +36,45 @@ public class QuestionController {
         return ResponseEntity.ok(questionRepository.findAll());
     }
 
-    @PostMapping
+    @GetMapping("/{questionId}")
     @CrossOrigin
-    public ResponseEntity<Question> create(@RequestBody Question question) {
-        Question newQuestion = questionRepository.save(question);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newQuestion);
+    public ResponseEntity<?> readById(@PathVariable Integer questionId) {
+        Question question = questionRepository.findById(questionId).orElse(null);
+        if (question != null) {
+            return ResponseEntity.ok(question);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question id " + questionId + " not found!");
+    }
+
+    @PutMapping("/{questionId}")
+    @CrossOrigin
+    public ResponseEntity<?> update(@PathVariable Integer questionId, @RequestBody Question updatedQuestion) {
+        Question question = questionRepository.findById(questionId).orElse(null);
+        if (question != null) {
+
+            List<Choice> choices = question.getChoices();
+            for (int i = 0; i < choices.size(); i++) {
+                question.getChoices().get(i).setText(updatedQuestion.getChoices().get(i).getText());
+                question.getChoices().get(i).setCorrect(updatedQuestion.getChoices().get(i).getCorrect());
+            }
+            question.setTopic(updatedQuestion.getTopic());
+            question.setSubject(updatedQuestion.getSubject());
+            question.setGrade(updatedQuestion.getGrade());
+            question.setTitle(updatedQuestion.getTitle());
+            questionRepository.save(question);
+            return ResponseEntity.ok(question);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question id " + questionId + " not found!");
+    }
+
+    @DeleteMapping("/{questionId}")
+    @CrossOrigin
+    public ResponseEntity<?> delete(@PathVariable Integer questionId) {
+        Question question = questionRepository.findById(questionId).orElse(null);
+        if (question != null) {
+            questionRepository.delete(question);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Question id " + questionId + " deleted!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question id " + questionId + " not found!");
     }
 }
